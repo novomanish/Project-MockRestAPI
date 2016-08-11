f71c66e5b7e1020f2177691c0105a55c0bed2e2c
Manish Jaiswal <novomanish@gmail.com>
2016-08-11 18:14:38 +0530

    Implement DataSource

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,33 @@
 var express = require('express');
+var ds = require('../lib/DataSource');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express5' });
 });
 
+function handleResponse(resource, res){
+    if(resource.headers){
+        res.set(resource.headers);
+    }
+    res.send(resource.body);
+}
 router.get('/*', function(req, res, next) {
-  res.render('index', { title: req.url });
+    ds.get(req.url, function(resource){
+        handleResponse(resource, res);
+    });
+});
+
+router.put('/*', function(req, res, next) {
+    var resource = ds.put({
+        "key": req.url,
+        "body": JSON.stringify(req.body),
+        "callback": function(resource){
+            handleResponse(resource, res);
+        }
+    });
 });
 
 module.exports = router;
